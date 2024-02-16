098c3db418b10c7ee809e9886559e8a07c19627f
noppadolr <riyasarn.n@gmail.com>
2024-02-16 17:23:35 +0700

    Define Routing

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,9 @@ Route::get('/', function () {
 
 
 Route::get('about', function () {
-    return "<h1>About Page</h1>";
+    return "<h1> About Page </h1>";
+});
+
+Route::get('contact',function(){
+    return "<h1>Contact Page</h1>";
 });
