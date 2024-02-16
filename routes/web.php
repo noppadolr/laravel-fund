d9ec82712c344ff505ce0e472efed243a415ed14
noppadolr <riyasarn.n@gmail.com>
2024-02-16 17:52:30 +0700

    Route Naming

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,24 @@ Route::get('/', function () {
 
 Route::get('about', function () {
     return "<h1> About Page </h1>";
-});
+})->name('about');
 
 Route::get('contact',function(){
     return "<h1>Contact Page</h1>";
 });
 
+// #Route Parameters
 Route::get('contact/{id}',function($id){
     return $id;
+})->name('edit-contact');
+
+
+
+// #Route Naming
+// Route::get('home',function(){
+//     return "<a href='".route('about')."'>About</a>";
+// });
+
+Route::get('home',function(){
+    return "<a href='".route('edit-contact',12)."'>About</a>";
 });
